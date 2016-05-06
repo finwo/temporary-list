2ab9f26fa491ee3d246404cfe3bcacf5d7008df4
Robin Bron <admin@finwo.nl>
2016-05-07 00:21:31 +0200

    Changed memcached server

--- app/services.php
+++ app/services.php
@@ -2,6 +2,6 @@
 
 registerService('cache', function() {
     $cache = new \Memcached();
-    $cache->addServer('127.0.0.1', 11211);
+    $cache->addServer('memcached', 11211);
     return $cache;
 });
